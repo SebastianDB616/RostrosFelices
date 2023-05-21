1ddc33172677f369aa7fc9be9859f9a4e128c9f8
Sebastian Dorado <ceceptrabajos123@gmail.com>
2023-05-21 15:13:02 -0500

    Commit para todas las carpetas que se encuentran en la dirección "Pages"

---
+++ Pages/Servicios/Borrar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RostrosFelices.Pages.Servicios
+{
+    public class BorrarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Servicios/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RostrosFelices.Pages.Servicios
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
