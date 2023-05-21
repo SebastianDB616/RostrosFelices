6b42f9f29d15aff545e43472714259a2dbfa199c
Sebastian Dorado <ceceptrabajos123@gmail.com>
2023-05-21 15:13:16 -0500

    Commit configuración Program.cs

--- Program.cs
+++ Program.cs
@@ -14,12 +14,18 @@ namespace RostrosFelices
             // Add services to the container.
             builder.Services.AddRazorPages();
 
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("RostrosFelicesDB"))
+            );
+
             builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-              .AddCookie(options =>
-                 {
-                     options.LoginPath = "/Login";
-                     options.AccessDeniedPath = "/AccessDenied";
-                 });
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/Account/Login";
+                    options.AccessDeniedPath = "/Account/AccessDenied";
+                });
+
+            builder.Services.AddAuthorization();
 
             var app = builder.Build();
 
@@ -28,6 +34,7 @@ namespace RostrosFelices
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapRazorPages();
