a5d14fd5ed82af59de95b7b60dcce7115c81372b
Sebastian Dorado <ceceptrabajos123@gmail.com>
2023-05-21 14:20:08 -0500

    Configuración del DbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RostrosFelices.Models;
+
+namespace RostrosFelices.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Servicio> Servicios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=RostrosFelicesRF;Trusted_Connection=True;");
+        }
+    }
+}
